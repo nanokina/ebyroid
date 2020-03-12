3644949ff8f9a77dd12d8499ba996c4062fbc907
Kinas <24854132+nanokina@users.noreply.github.com>
2020-03-12 12:52:14 +0900

    Feat update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const Ebyroid = require('./lib/ebyroid');
+const Voiceroid = require('./lib/voiceroid');
+const MiniServer = require('./lib/mini_server');
+const WaveObject = require('./lib/wave_object');
 
 module.exports = {
   Ebyroid,
+  Voiceroid,
+  MiniServer,
+  WaveObject,
 };
